1c44fa739bed73c596236b4e12b4a09ff2cbd6a6
Aubhro <aubhrosengupta@gmail.com>
2017-01-14 12:34:36 -0800

    Document net

--- ml.py
+++ ml.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import svm
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 
 X_train = np.array([[0, 0],
                     [2, 3],
@@ -8,6 +8,7 @@ X_train = np.array([[0, 0],
                     [8, 8],
                     [6, 9]])
 
+# Classes 0, 1
 y_train = np.array([0, 0, 1, 1, 1])
 
 X_test = np.array([[9, 9], [-1, -1]])
@@ -18,4 +19,6 @@ clf = svm.SVC()
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
-print classification_report(y_test, y_pred)
+
+print len(y_pred)
+print accuracy_score(y_test, y_pred)
--- net.py
+++ net.py
@@ -3,6 +3,15 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+# Stores placeholder of unspecified size for training samples of size 784
+x = tf.placeholder(tf.float32, [None, 784])
 
+# Creates 10 nodes so outputs are 10 long
 weights = tf.Variable(tf.zeros([784, 10]))
-print weights
+biases = tf.Variable(tf.zeros([10]))
+
+# Constructs softmax model
+y = tf.nn.softmax(tf.matmul(x, weights) + biases)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
